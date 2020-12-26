4a3d0a9ab7191a6afd2507075a0b1ec76b7b68c0
tenfrs <podaand@icloud.com>
2020-12-26 22:17:51 +0500

    Задание 10.1 Поспелова Дарья

--- 10-java-functional-stream/task01/src/com/example/task01/Task01Main.java
+++ 10-java-functional-stream/task01/src/com/example/task01/Task01Main.java
@@ -3,14 +3,13 @@ package com.example.task01;
 import java.io.IOException;
 
 public class Task01Main {
-    public static void main(String[] args) throws IOException {
-
+{
+        throw new NullPointerException();
     }
 
     if(condition ==null||ifTrue ==null||ifFalse ==null)
+    public static void main(String[] args) throws IOException {
 
-    {
-        throw new NullPointerException();
     }
         return x ->condition.test(x)?ifTrue.apply(x):ifFalse.apply(x);
 }
