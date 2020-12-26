8112122937aab4d94e9196933f0ae302920471c0
tenfrs <podaand@icloud.com>
2020-12-26 13:14:51 +0500

    Поспелова Дарья Задание 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -5,11 +5,11 @@ public class Task02 {
     public static String solution(String input) {
 
         long line = Long.parseLong(input);
-        if (Byte.MIN_VALUE <= line && line <= Byte.MAX_VALUE)
+        if(Byte.MIN_VALUE <= line && line <= Byte.MAX_VALUE)
             return "byte";
-        if (Short.MIN_VALUE <= line && line <= Short.MAX_VALUE)
+        if(Short.MIN_VALUE <= line && line <= Short.MAX_VALUE)
             return "short";
-        if (Integer.MIN_VALUE <= line && line <= Integer.MAX_VALUE)
+        if(Integer.MIN_VALUE <= line && line <= Integer.MAX_VALUE)
             return "int";
         return "long";
     }
